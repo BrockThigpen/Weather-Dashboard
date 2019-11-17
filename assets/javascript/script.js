a10f9914ac92e827eeebe6df75aad2a8b328969b
Brock T <brockthigpen@yahoo.com>
2019-11-17 15:34:34 -0500

    created form submit event, started building queryURL and ajax

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,18 @@
-// apikey: 3b5715c4cc3d80c499e1192a073d5982
 // api.openweathermap.org/data/2.5/forecast?q=charlotte,us&mode=xml&appid=3b5715c4cc3d80c499e1192a073d5982
-// let queryUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + userInput + ',us&mode=xml&appid=3b5715c4cc3d80c499e1192a073d5982'
-// let userInupt = $('#userInput').val().trim();
+
+
+function buildQueryUrl(){
+    let queryUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + userInput + ',us&mode=xml&appid=3b5715c4cc3d80c499e1192a073d5982'
+
+}
+// submit event for search area
+$('#search').submit(function(e){
+    e.preventDefault();
+    console.log($("#userInput").val());
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(updatePage);
+    $("#userInput").val('');
+})
