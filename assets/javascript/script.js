51d0d4cab682504f88978cc5d7a4fe31e83391df
Brock T <brockthigpen@yahoo.com>
2019-11-17 15:03:38 -0500

    added html structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,4 @@
 // apikey: 3b5715c4cc3d80c499e1192a073d5982
 // api.openweathermap.org/data/2.5/forecast?q=charlotte,us&mode=xml&appid=3b5715c4cc3d80c499e1192a073d5982
+// let queryUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + userInput + ',us&mode=xml&appid=3b5715c4cc3d80c499e1192a073d5982'
+// let userInupt = $('#userInput').val().trim();
